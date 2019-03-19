1c346fcb4e0fa17b9bbc9234ff3372465957a151
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-03-19 16:52:09 -0400

    Loaded store 1 & 2 and updated store 1's name

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,7 +1,20 @@
 require_relative '../setup'
 require_relative './exercise_1'
 
-puts "Exercise 2"
+puts "\nExercise 2"
 puts "----------"
 
-# Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+puts "\nLoad store 1: #{@store1.name}"
+
+@store2 = Store.find_by(id: 2)
+puts "\nLoad store 2: #{@store2.name}"
+
+@store1 = Store.find_by(id: 1)
+@store1.name = 'Winners'
+@store1.save
+
+puts "\nUpdate store 2: #{@store1.name}"
+
+
