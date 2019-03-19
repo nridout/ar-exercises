c87405dd46087fb2c436d612ad5d57f6572e563c
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-03-19 16:42:16 -0400

    Added 3 stores to db

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,12 @@
 require_relative '../setup'
 
-puts "Exercise 1"
+puts "\nExercise 1"
 puts "----------"
 
-# Your code goes below here ...
+# store = Store.create(name: string, annual_revenue: integer, mens_apparel: boolean, womens_apparel :boolean)
+
+store1 = (Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true))
+store2 = (Store.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true))
+store1 = (Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false))
+
+puts  "\nThere are #{Store.count} stores in the database"
