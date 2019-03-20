8fb10da83be5967e3cb479791de646d280dfc789
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-03-20 09:42:20 -0400

    Added stores to db and custom searches

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -3,7 +3,30 @@ require_relative './exercise_1'
 require_relative './exercise_2'
 require_relative './exercise_3'
 
-puts "Exercise 4"
+puts "\nExercise 4"
 puts "----------"
 
-# Your code goes here ...
+# Create 3 more stores
+Store.create(name: 'Surrey', annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: 'Whistler', annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+# Test output to check stores have been added
+puts  "\nThere are #{Store.count} stores in the database"
+
+# Look up the stores that carry men's apparel
+@mens_stores = Store.where(mens_apparel: true)
+
+# Loop through the men's stores, printing name and revenue
+@mens_stores.each do |store|
+  puts "Store Name: #{store.name} Annual Revenue: #{store.annual_revenue}"
+end
+
+# Look up the stores that carry women's apparel & less than $1M
+@womens_stores = Store.where("womens_apparel = true AND annual_revenue < 1000000")
+
+# Loop through the women's stores, printing name and revenue
+@womens_stores. each do |store|
+  puts "Store Name: #{store.name} Annual Revenue: #{store.annual_revenue}"
+end
+
